73622680cb7cd29282c6a3f3714b4ee51b3eead7
tungpmshe130687@gmail.com <tungpmshe130687@gmail.com>
2023-04-20 21:09:22 +0700

    update 20/4

--- javaScript.js
+++ javaScript.js
@@ -31,57 +31,92 @@ document.getElementById('form').addEventListener('submit',function(event)
     event.preventDefault(); // Ngăn chặn hành vi submit form mặc định
 
     var minutes = document.getElementById('enter-minutes').value; // Get value of Minutes countdown
-    
+    let secondsInput = parseInt(minutes)*60; // Parse to seconds
+
+    var valueSelected = document.getElementById('selectTypes').value;
+    const pattern=/^[1-9]\d*$/;
+
+   //  console.log(pattern.test(minutes));
+
+    if(valueSelected === "default" && minutes === ''){
+       alert('Please chose one option to start');
+       document.getElementById('enter-minutes').value = "";
+       return;
+    }
+    else if(!pattern.test(minutes) && valueSelected === "default")
+    {
+      confirm('Please enter a positive number');
+      document.getElementById('enter-minutes').value = "";
+      return; 
+    }else if(pattern.test(minutes) && valueSelected === "default")
+    {
+
+    }
+    else 
+    {
+      secondsInput = Number(valueSelected);
+    }
+   
+
+
     var countDownText = document.getElementById('timeCountDown'); 
 
     // Reset value input
     document.getElementById('enter-minutes').value = "";
     
     //Animation
-    document.getElementById('form').style.transform = 'translateY(-18rem)';
-    document.querySelector('.display').style.transform  = 'translateX(0)';
+    document.getElementById('form').classList.add('active');
+    document.querySelector('.display').classList.add('active');
+    document.querySelector('.display').classList.remove('fadeOut');
     
     // COUNTDOWN TIME 
     var btn_Cancel = document.getElementById('btn-countdown-cancel');
     var audioAlarm = document.getElementById('myAudio');
-    let secondsInput = parseInt(minutes)*60;
 
-    let countDownInterval = setInterval(function countDown(){
-        if(secondsInput < 0)
-        {
-         clearInterval(countDownInterval);
+    let countDownInterval  = setInterval(function()
+    {
+      countDown(secondsInput);
+    }  ,1000);;
 
-         audioAlarm.play();
-
-         document.querySelector('.alarm-icon').classList.add('active');
+     
 
 
-         setTimeout(function(){
-            audioAlarm.pause();
-         }, duration * 1000);
-        }
-        else{
-         const hoursC = Math.floor(secondsInput/3600);
-         const minutesC = Math.floor(Number(secondsInput/60) - hoursC*60);
-         const secondC = secondsInput % 60;
+    function countDown(se){
+      if(se < 0)
+      {
+       clearInterval(countDownInterval);
+       audioAlarm.play();
 
-         countDownText.innerHTML = `
-         Counting  <span>${String(hoursC).padStart(2,'0')} : ${String(minutesC).padStart(2,'0')} : ${String(secondC).padStart(2,'0')} </span>
-         `
-         secondsInput = secondsInput - 1;
-        }
-     },1000);
+       document.querySelector('.alarm-icon').classList.add('active');
+
+       setTimeout(function(){
+          audioAlarm.pause();
+       }, duration * 1000);
+      }
+      else{
+       const hoursC = Math.floor(se/3600);
+       const minutesC = Math.floor(Number(se/60) - hoursC*60);
+       const secondC = se % 60;
+
+       countDownText.innerHTML = `
+       Counting  <span>${String(hoursC).padStart(2,'0')} : ${String(minutesC).padStart(2,'0')} : ${String(secondC).padStart(2,'0')} </span>
+       `
+       secondsInput = secondsInput - 1;
+      }
+   }
 
      btn_Cancel.addEventListener('click',
      function(){
         clearInterval(countDownInterval);
         audioAlarm.pause();
         document.querySelector('.alarm-icon').classList.remove('active');
-        document.getElementById('form').style.transform = 'translateY(0)';
-        document.querySelector('.display').style.transform  = 'translateX(-100rem)';
+        document.getElementById('form').classList.remove('active');     
+        document.querySelector('.display').classList.remove('active');
+        document.querySelector('.display').classList.add('fadeOut');
      });
-    
 });
+
+
 
 
 
